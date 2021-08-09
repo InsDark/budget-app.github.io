9c23887b4269d1099a02c962f59487fd53938bc0
InsDark <65677103+InsDark@users.noreply.github.com>
2021-08-08 21:15:52 -0500

    function-way

--- app.js
+++ app.js
@@ -1,100 +1,159 @@
-class Expense {
-    constructor (title, value) {
-        this.title = title;
-        this.value = value;
-    }
+let getBudget = document.querySelector('#getBudget')
+
+    getBudget.addEventListener('submit', e => {
+        e.preventDefault()
+        let budget = document.querySelector('.budget').value
+        let allBudget = document.querySelector('.currentBudget')
+        allBudget.innerHTML = `$ ${budget}`
+        showMessage('The budget has been updated successfully', 'green')
+        getBudget.reset()
+        updatedBalance()
+    })
+
+let getExpense = document.querySelector('#getExpense')
+    getExpense.addEventListener('submit', e => {
+        e.preventDefault()
+        const expenseTitle = document.querySelector('.expenseTitle').value
+        const expenseValue = document.querySelector('.expenseAmount').value
+    
+        if(expenseTitle.trim() === '' || expenseTitle.trim() === null) {
+            return showMessage('Please write a correct expense title', 'red')
+        } else if( isNaN(expenseTitle.trim()) === false ){
+            return showMessage('Please dont start with a number the expense title', 'red')
+        }
+    
+    showMessage('The expense list has been added successfully', 'green')
+    getExpense.reset()
+    updatedExpenseList(expenseTitle, expenseValue)
+ })
+
+const showMessage = (message, cssColor) => {
+    const div = document.createElement('div')
+    div.style.background = cssColor
+    div.style.color = 'white'
+    div.innerHTML = `${message}`
+    div.style.width = '100%'
+    div.style.padding = '1rem'
+    div.style.textAlign = 'center'
+    div.style.position = 'fixed'
+    const app = document.querySelector('.app')
+    document.body.insertBefore(div, app)
+    setTimeout(() =>{
+        div.remove()
+    }, 2000)
 }
 
-class UI {
-    addExpense (expense) {
-        const expensesList = document.querySelector('.expenseDetails')
-        const newExpense = document.createElement('div')
+const updatedExpenseList = (expenseTitle, expenseValue) => {
+    const expenseList = document.querySelector('.expenseDetails')
+    const newExpense = document.createElement('div')
         newExpense.classList.add('newExpense')
-        newExpense.innerHTML = `<h5>${expense.title}</h5>
-                                <p>$ ${expense.value}</p>
+        newExpense.innerHTML = `<h5>${expenseTitle}</h5>
+                                <p>$${expenseValue}</p>
                                 <div>
                                     <i class='fa fa-edit'></i>
                                     <i class='fas fa-trash'></i>
                                 </div>`
-        expensesList.appendChild(newExpense)
-        this.sumExepenses(expense.value)
-    }
-    deleteExpense(element){
-        // element
-        if(element.classList.contains('fa-trash')){
-            element.parentElement.parentElement.remove()
-        }
-    }
-    showMessage(message, cssColor) {
-        const div = document.createElement('div')
-        div.style.background = cssColor
-        div.style.color = 'white'
-        div.innerHTML = `${message}`
-        div.style.width = '100%'
-        div.style.padding = '1rem'
-        div.style.textAlign = 'center'
-        div.style.position = 'fixed'
-        const app = document.querySelector('.app')
-        document.body.insertBefore(div, app)
-        setTimeout(() =>{
-            div.remove()
-        }, 2000)
-    }
-    resetForm(value) {
-        if(value === true){
-            document.querySelector('#getExpense').reset()
-        } else{
-            document.querySelector('#getBudget').reset()
-        }
-    }
-    updatedBudget() {
-
-    }
-    sumExepenses(value) {
-        expense + parseInt(value)
-    }
-    resExpenses(value){
-        expense - parseInt(value)
-    }
+        expenseList.appendChild(newExpense)
+    updatedExpenseValue(expenseValue)
 }
 
-let expense = 0;
-document.querySelector('#getBudget')
-    .addEventListener('submit', e => {
-        e.preventDefault()
-        const budget = document.querySelector('.budget').value
-        const ui = new UI()
-        ui.showMessage('The budget has been updated successfully', 'green')
-        ui.resetForm()
+let plus = 0;
+
+const updatedExpenseValue = (value) => {
+    let currentExpenseAmount = document.querySelector('.currentExpenseAmount')
+    currentExpenseAmount.innerHTML = `$ ${plus += parseFloat(value)}`
+    updatedBalance()
+}
+
+document.querySelector('.expenseDetails').addEventListener('click', e => {
+    if(e.target.classList.contains('fa-trash')) {
+        const newValue = e.target.parentElement.parentElement.children[1].textContent
+
+        let newArr = newValue.split('')
+        newArr.shift(); 
+
+        let newLetter = ''
+        newArr.forEach(letter => {
+            newLetter += letter
+            return newLetter
+        })
+
+        updatedExpenseValue(-newLetter)
+
+        e.target.parentElement.parentElement.remove()
+        showMessage('The expense item you clicked was deleted', 'red')
+    }
 })
 
-document.querySelector('#getExpense')
-    .addEventListener('submit', e => {
-        e.preventDefault()
-        const expenseTitle = document.querySelector('.expenseTitle').value
-        const expenseValue = document.querySelector('.expenseAmount').value
-        console.log(isNaN(expenseTitle.trim()))
+document.querySelector('.expenseDetails').addEventListener('click', e => {
+    if(e.target.classList.contains('fa-edit')) {
+        const newValue = e.target.parentElement.parentElement.children[1].textContent
 
-        const ui = new UI()
+        let newArr = newValue.split('')
+        newArr.shift(); 
 
-        if(expenseTitle.trim() === '' || expenseTitle.trim() === null) {
-            return ui.showMessage('Please write a correct expense title', 'red')
-        } else if( isNaN(expenseTitle.trim()) === false ){
-            return ui.showMessage('Please dont start with a number the expense title', 'red')
-        }
+        let newLetter = ''
+        newArr.forEach(letter => {
+            newLetter += letter
+            return newLetter
+        })
 
-        const expense = new Expense (expenseTitle, expenseValue)
+        updatedExpenseValue(-newLetter)
+        
+        
+        let hereTitle = e.target.parentElement.parentElement.children[0].textContent
+        let hereValue = e.target.parentElement.parentElement.children[1].textContent
 
-        ui.addExpense(expense)
-        ui.showMessage('The expense has been added successfully', 'green')
-        ui.resetForm(true)
+        let newHereValue =  hereValue.split('')
+        newHereValue.shift()
+        let allNumbers = '';
+        newHereValue.forEach(number => {
+            allNumbers += number
+            return allNumbers
+        })
+
+
+        const expenseTitle = document.querySelector('.expenseTitle')
+        const expenseValue = document.querySelector('.expenseAmount')
+
+        expenseTitle.value = hereTitle
+        expenseValue.value = parseFloat(allNumbers)
+    
+        e.target.parentElement.parentElement.remove()
+        showMessage('The expense item is already to be changed', 'green ')
+    }
+})
+
+const updatedBalance = () => {
+
+    let currentExpenseAmount = document.querySelector('.currentExpenseAmount').textContent
+    const newArr = currentExpenseAmount.split('')
+    newArr.shift()
+    newArr.shift()
+    
+    let newLetter = ''
+    newArr.forEach(item => {
+        newLetter += item
+        return newLetter
     })
 
-document.querySelector('.expenseDetails')
-    .addEventListener('click', (e) => {
-        e.stopPropagation
-        const ui = new UI()
-        ui.deleteExpense(e.target)
-        ui.showMessage('The expense item has been deleted successfully', 'red')
-    })
+    let allBudget = document.querySelector('.currentBudget').textContent
+    const newBud = allBudget.split('')
+    newBud.shift()
+    newBud.shift()
 
+    let aLetter = ''
+    newBud.forEach(item => aLetter += item)
+
+    let newBalance = parseInt(aLetter) - parseFloat(newLetter)
+    let currentBalance = document.querySelector('.currentBalance')
+    currentBalance.innerHTML = `$ ${newBalance}`
+
+    if (newBalance > 0) {
+        currentBalance.style.color ='green'
+    } else if (newBalance < 0) {
+        currentBalance.style.color = 'red'
+    } else {
+        currentBalance.style.color = 'black'
+    }
+}
